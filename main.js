f231f6294106edd86fb027367ede7b0a77965e77
doctor82 <80136584+doctor82@users.noreply.github.com>
2022-11-29 19:30:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+song1="";
+song2="";
+function preload(){
+song1=loadSound("song1.mp3")
+song2=loadSound("song2.mp3")
+}
+function setup(){
+canvas=createCanvas(600,500);
+canvas.center();
+video=createCapture(VIDEO);
+video.hide();
+}
+function draw(){
+image(video,0,0,600,500);
+}
+function play(){
+song.play();
+song.setVolume(1);
+song.rate(1);
+}
