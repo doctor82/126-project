be5b3d72e8c3be2412d0a4dd13857b1751fef65e
doctor82 <80136584+doctor82@users.noreply.github.com>
2022-12-03 19:52:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 song1="";
+
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 function preload(){
 song1=loadSound("song1.mp3")
 song2=loadSound("song2.mp3")
@@ -9,6 +14,12 @@ canvas=createCanvas(600,500);
 canvas.center();
 video=createCapture(VIDEO);
 video.hide();
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+
+}
+function modelLoaded(){
+console.log("Posenet Is Intitialised");
 }
 function draw(){
 image(video,0,0,600,500);
@@ -17,4 +28,17 @@ function play(){
 song.play();
 song.setVolume(1);
 song.rate(1);
+}
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+leftWristX=results[0].pose.leftWrist.x;
+leftWristY=results[0].pose.leftWrist.y;
+rightWristX=results[0].pose.rightWrist.x;
+rightWristY=results[0].pose.rightWrist.y;
+console.log("Right Wrist X - "+rightWristX);
+console.log("left Wrist X - "+leftWristX);
+console.log("Right Wrist Y - "+rightWristY);
+console.log("left Wrist Y - "+leftWristY);
+}
 }
