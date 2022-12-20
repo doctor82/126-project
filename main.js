e9af5f4490ab5ede47d105851fc8c4afe25a0945
doctor82 <80136584+doctor82@users.noreply.github.com>
2022-12-20 19:22:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 function preload(){
-song1=loadSound("song1.mp3")
-song2=loadSound("song2.mp3")
+song1=loadSound("song1.mp3");
+song2=loadSound("song2.mp3");
 }
 function setup(){
 canvas=createCanvas(600,500);
@@ -29,16 +29,50 @@ song.play();
 song.setVolume(1);
 song.rate(1);
 }
-function gotPoses(results){
-if(results.length>0){
-console.log(results);
-leftWristX=results[0].pose.leftWrist.x;
-leftWristY=results[0].pose.leftWrist.y;
-rightWristX=results[0].pose.rightWrist.x;
-rightWristY=results[0].pose.rightWrist.y;
-console.log("Right Wrist X - "+rightWristX);
-console.log("left Wrist X - "+leftWristX);
-console.log("Right Wrist Y - "+rightWristY);
-console.log("left Wrist Y - "+leftWristY);
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score of Left Wrist: " + scoreLeftWrist);
+
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X  : " + leftWristX+", Left Wrist Y : "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X  : " + rightWristX+", Right Wrist Y : "+rightWristY);
+  
+    }
 }
+function draw() {
+    image(video, 0, 0, 600, 500);
+    song1_status=song1.isPlaying();
+    song2_status=song2.isNotPlaying();
+    fill("red");
+    stroke("black");
+    if(scoreRightWrist>0.2){
+        circle(rightWristX, rightWristY, 20);  
+    song2.stop();
+    if(song1_status==false){
+    song1.play();
+    document.getElementById("song").innerHTML="Playing Alone"
+    }
+    }
+    if (scoreLeftWrist>0.2) {
+        circle(leftWristX, leftWristY, 20);        
+        song1.stop();
+        if(song2_status==false){
+        song2.play();
+        document.getElementById("song").innerHTML="Playing AloneFaded";
+    }
+}   
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
